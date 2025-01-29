4e71da460fc4e8e3903270edc6cfd42f43e9ac1c
SouthFox <master@southfox.me>
2025-01-30 02:04:09 +0800

    [package] init racket

--- init.el
+++ init.el
@@ -176,7 +176,7 @@
         +pyright
         +tree-sitter)        ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
+       racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
