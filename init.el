1b2b9eced6dacb9934e0c70b9e95f9f8ada3a9b5
SouthFox <master@southfox.me>
2025-09-03 11:36:11 +0800

    [package] remove nix config

--- init.el
+++ init.el
@@ -164,9 +164,9 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       (nix
-        +lsp
-        +tree-sitter)               ; I hereby declare "nix geht mehr!"
+       ;; (nix
+       ;;  +lsp
+       ;;  +tree-sitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
         +roam2
